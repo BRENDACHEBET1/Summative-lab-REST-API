bfa804e248ba6b59e8b4a0d9413d76e2a3ae2d31
BRENDACHEBET1 <chebetb388@gmail.com>
2026-07-03 20:40:19 +0300

    Implement PATCH and DELETE routes

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 #Simulate in memorry data using a list
-products = []
+inventory = []
 
 #CRUD Operations
-#GET all products
-@app.route('/products', methods = ["GET"])
-def get_products():
-    return jsonify(products), 200
+#GET all inventory
+@app.route('/inventory', methods = ["GET"])
+def get_inventory():
+    return jsonify(inventory), 200
 
 #Get a single product
-@app.route('/products/<int: id>', methods= ["GET"])
-def get_product(id):
-    for p in products:
-        if p["id"] == id:
-            return jsonify(p), 200
+@app.route('/inventory/<int:item_id>', methods= ["GET"])
+def get_product(item_id):
+    for item in inventory:
+        if item["id"] == item_id:
+            return jsonify(item), 200
         
     return jsonify({"error": "Product not found"}), 404
 
 #Add a product
-@app.route('/products', methods=["POST"])
+@app.route('/inventory', methods=["POST"])
 def add_product():
     data = request.get_json()
-    new_id = max((p.id for p in products), default=0) + 1
+    new_id = max((item.id for item in inventory), default=0) + 1
 
     item = {
         "id": new_id,
@@ -36,7 +36,26 @@ def add_product():
         "source": "manual"
     }
 
-    products.append(item)
+    inventory.append(item)
     
 
     return jsonify(item), 201
+
+#Update product
+@app.route('inventory/<int:item_id>', methods=["PATCH"])
+def update_product(item_id):
+    data = request.get_json()
+
+    for item in inventory:
+        if item["id"] == item_id:
+            item.upadte(data)
+            return jsonify(item)
+        
+    return("Not found", 404)
+
+#DElete product
+@app.route("/items/<int:item_id>", methods=["DELETE"])
+def delete_item(item_id):
+    global inventory
+    inventory = [i for i in inventory if i["id"] != item_id]
+    return jsonify({"message": "deleted"})
