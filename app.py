ac50a9ed91cd4cc61f3ddff4df510e434b7142e1
BRENDACHEBET1 <chebetb388@gmail.com>
2026-07-03 20:22:06 +0300

    Implement POST/products route

--- app.py
+++ app.py
@@ -20,3 +20,23 @@ def get_product(id):
         
     return jsonify({"error": "Product not found"}), 404
 
+#Add a product
+@app.route('/products', methods=["POST"])
+def add_product():
+    data = request.get_json()
+    new_id = max((p.id for p in products), default=0) + 1
+
+    item = {
+        "id": new_id,
+        "name": data["name"],
+        "barcode": data.get("barcode"),
+        "quantity": data["quantity"],
+        "price": data["price"],
+        "category": data["category"],
+        "source": "manual"
+    }
+
+    products.append(item)
+    
+
+    return jsonify(item), 201
